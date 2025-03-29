104bc693e4f2e8613dad4f3357180b9421a09567
mayouspublishing <admin@mayous.org>
2025-03-29 12:45:08 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 export default {
   async fetch(request) {
     const url = new URL(request.url);
-
-    // If the URL contains ?m=1 or &m=1, remove it and redirect
-    if (url.searchParams.get("m") === "1") {
+    const cookieHeader = request.headers.get("Cookie") || "";
+    
+    // If the URL has ?m=1 and we haven't already cleaned it (no cookie set)
+    if (url.searchParams.get("m") === "1" && !cookieHeader.includes("cleaned=true")) {
       url.searchParams.delete("m");
-      return Response.redirect(url.toString(), 301);
+      const newUrl = url.toString();
+      
+      // Create a redirect response to the clean URL
+      const response = Response.redirect(newUrl, 301);
+      
+      // Set a cookie so that subsequent requests won't be redirected again
+      response.headers.append("Set-Cookie", "cleaned=true; Path=/; Max-Age=3600");
+      return response;
     }
-
+    
+    // Otherwise, just fetch the request as is
     return fetch(request);
   }
-}
+};
